dba99a8e95d15846724a8f79c21226033a27fc2e
phoenix16 <c.prakriti@gmail.com>
2013-05-06 00:50:52 -0700

    Update classification.cpp

--- classification.cpp
+++ classification.cpp
@@ -1,26 +1,13 @@
+/*
+ * Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
+*/
+
 #include "classification.h"
-
-classification::classification()
-{
-}
-
-
-
 
 void classification::useSVM(const Mat& trainData, const Mat& trainLabels, const Mat& testData, Mat& responses)
 {
     // Set up SVM parameters
     CvSVMParams SVM_params;
-//    SVM_params.svm_type = CvSVM::C_SVC;
-//    SVM_params.kernel_type = CvSVM::LINEAR;
-//    SVM_params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 1000, 0.01);
-    // If non-linear kernel:
-    //SVM_params.degree = 0;
-    //SVM_params.gamma = 1;
-    //SVM_params.coef0 = 0;
-    //SVM_params.C = 1;
-    //SVM_params.nu = 0;
-    //SVM_params.p = 0;
 
     // Train the SVM
     CvSVM SVM;
