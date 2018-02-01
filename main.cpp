e4f85c2ea24244bf7cb025ed9264031b5f560090
Prakriti C <c.prakriti@gmail.com>
2018-02-01 15:24:28 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-/*
- * Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
-*/
+//
+// Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
+//
 
 #include "bowfeatures.h"
 #include "classification.h"
