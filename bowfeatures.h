ea667a1a2fe7226cfc110a7e304eaee399146309
Prakriti C <c.prakriti@gmail.com>
2018-02-01 15:23:50 -0800

    Update bowfeatures.h

--- bowfeatures.h
+++ bowfeatures.h
@@ -1,6 +1,6 @@
-/*
- * Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
-*/
+//
+// Developer : Prakriti Chintalapoodi - c.prakriti@gmail.com 
+//
 
 #ifndef BOWFEATURES_H
 #define BOWFEATURES_H
